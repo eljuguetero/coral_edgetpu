6a5442975126b107ea90580408bebef0aa8285ac
Andres Calderon <andresfcalderon@gmail.com>
2021-03-10 09:27:00 -0500

    ultraface slim model added

--- src/image_preprocessing/img_prep.cc
+++ src/image_preprocessing/img_prep.cc
@@ -9,7 +9,7 @@ namespace edge
 	std::vector<uint8_t> GetInputFromImage(cv::Mat input_frame, const int& width, const int& height, const int& channels)
 	{
 		cv::resize(input_frame, input_frame, cv::Size(width,height));
-		cv::cvtColor(input_frame,input_frame,CV_BGR2RGB);
+		cv::cvtColor(input_frame,input_frame,cv::COLOR_BGR2RGB);
 		cv::Mat flat = input_frame.reshape(1, input_frame.total()*input_frame.channels());
 		std::vector<uint8_t> in_vec= input_frame.isContinuous()? flat : flat.clone();
 		return in_vec;
